193c2a25918e74f94022868ddef9c1e7b2694054
mauro saavedra <saa.mauro92@gmail.com>
2022-12-07 23:41:48 +0000

    fix: cors configs

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const bp = require('body-parser')
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
-app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization");
+app.use(cors({
+  origin: "*",
+  methods: "GET, POST, PATCH, DELETE, PUT",
+  allowedHeaders: "Content-Type, Authorization",
+}));
 
-    next();
-  });
 
 
 //
